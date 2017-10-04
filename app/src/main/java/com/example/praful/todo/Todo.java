145ce5e0ae7b223d2b3fbd2104837df38dca30e7
Praful Dhabekar <pddhabekar@gmail.com>
2017-10-04 16:17:57 +0530

    Some code added

---
+++ app/src/main/java/com/example/praful/todo/Todo.java
@@ -0,0 +1,13 @@
+package com.example.praful.todo;
+
+import java.io.Serializable;
+
+/**
+ * Created by praful on 10/4/2017.
+ */
+
+public class Todo implements Serializable {
+
+    public String content;
+    public boolean done;
+}
---
+++ app/src/test/java/Todo.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+
+/**
+ * Created by praful on 10/4/2017.
+ */
+
+public class Todo implements Serializable {
+
+    public String content;
+    public boolean done;
+}
